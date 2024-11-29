e4934a6f4d136927c20456ce7add74faee51becc
Denis <dovzhanyndenis03@gmail.com>
2024-11-29 13:14:52 -0500

    feat: changing the way sending chats work / over websocket instead of api

--- lib/chattr_web/channels/chat_channel.ex
+++ lib/chattr_web/channels/chat_channel.ex
@@ -1,12 +1,25 @@
 defmodule ChattrWeb.ChatChannel do
+  alias Chattr.Messages.Message
+  alias Chattr.Messages
   alias Chattr.UserChat
+  alias Chattr.Chats
   use Phoenix.Channel
 
   def join("chat:" <> chat_id, _params, socket) do
     case Chats.get_chat_by_user_and_chat_id(%{"user_id" => socket.assigns["user_id"], "chat_id" => chat_id}) do
       %UserChat{} ->
-        {:ok, socket}
+        {:ok, assign(socket, "chat_id", chat_id)}
 
+      _ -> {:error, %{"reason" => "unauthorized"}}
+    end
+  end
+
+  def handle_in("new_msg", %{"content" => content}, socket) do
+    case Messages.create_message(%{"user_id" => socket.assigns["user_id"], "chat_id" => socket.assigns["chat_id"], "content" => content}) do
+      %Message{} ->
+        broadcast!(socket, "new_msg", %{"content" => content})
+        {:noreply, socket}
+        
       _ -> {:error, %{"reason" => "unauthorized"}}
     end
   end
