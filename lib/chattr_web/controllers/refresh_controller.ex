2789d5d69f7bc6cf29d88349458f20d420cc3c46
Denis <dovzhanyndenis03@gmail.com>
2025-03-08 16:45:18 -0500

    feat: whenever a user tries to refresh their access token, we will check whether their refresh token is also within a day of expiring, if it is valid and within a day we issue a new refresh token along with the access

--- lib/chattr_web/controllers/refresh_controller.ex
+++ lib/chattr_web/controllers/refresh_controller.ex
@@ -15,6 +15,19 @@ defmodule ChattrWeb.RefreshController do
                  "exp" => Joken.current_time() + 900,
                  "token_type" => "access"})
 
+
+              ## check to see whether its ab to expire, if it is we refresh the refresh!
+              conn = if is_number(claims["exp"]) && claims["exp"] - Joken.current_time() < 86400 do
+                refresh_token = Auth.generate_and_sign!(%{"user_id" => claims["user_id"],
+                  "exp" => Joken.current_time() + 604800,
+                  "token_type" => "refresh"})
+      
+                Redix.command(:redix, ["SET", "refresh_token:#{claims["user_id"]}", refresh_token, "EX", "604800"])
+                put_resp_cookie(conn,"refresh_token", refresh_token, http_only: true, secure: true, same_site: "strict", max_age: 604800 )
+              else
+                conn
+              end
+
               conn
               |> put_status(:ok)
               |> json(%{"access_token" => new_token})
